dc79711b1a4865c6ffa245adcd6bd7509f9b996e
fernancat <fernancarranza90@gmail.com>
2024-04-05 20:21:24 -0600

    arreglando

---
+++ desafio2/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program{
+    public static void Main(string[] args){
+
+        int [,] numeros = new int[5,5];
+        
+        CalcularCompras(numeros);
+
+        Console.ReadKey(true);
+    }
+
+
+    //metodo que reciba un arreglo bidimensional
+
+
+
+    public static void CalcularCompras(int[,] listaCompras){
+            int[] precioFinalClientes = new int [ listaCompras.GetLength(0)];
+
+
+
+            for (int i = 0; i < listaCompras.GetLength(0);i++){
+                for (int j = 0; j < listaCompras.GetLength(1);j++){
+                    Console.WriteLine($"Ingresa el precio del cliente {i +1} de la Compra No.{j+1}: ");
+                    listaCompras[i,j] = int.Parse(Console.ReadLine());
+                    precioFinalClientes[i] += listaCompras[i,j];
+
+                
+                }
+            }
+
+
+            for ( int i = 0; i < precioFinalClientes.Length; i++){
+                if (precioFinalClientes[i] <= 99){
+                    Console.WriteLine($" cliente {i+1} no aplica descuento");
+                }
+
+                else if (precioFinalClientes[i] >=100 && precioFinalClientes[i]<=1000){
+                    Console.WriteLine($" cliente {i+1} si aplico al descuento del 10% que tendria un precio final de: { precioFinalClientes[i]-(precioFinalClientes[i] * 0.1) }");
+
+                }
+                else if (precioFinalClientes[i] >=1000){
+                    Console.WriteLine($" cliente {i+1} si aplico al descuento del 20% que tendria un precio final de: {precioFinalClientes[i]-(precioFinalClientes[i] * 0.2)}");
+
+                }
+
+            }
+        
+
+
+    }
+
+
+}
---
+++ desafio3/Program.cs
@@ -0,0 +1,65 @@
+﻿public class Program{
+    public static void Main(string[] args){
+
+Console.WriteLine("hola");
+        bool validacion = true;
+        int opcion;
+        List<string> lista = new List<string>(){};
+
+        do{
+            Console.Clear();
+            Console.WriteLine(@"
+                -----------------------
+                Menu Administrador
+                ----------------------
+
+                1. Mostrar
+                2. Agregar
+                3. Eliminar
+                4. Salir
+            ");
+
+
+
+            opcion = int.Parse(Console.ReadLine());
+
+            switch(opcion){
+                case 1: 
+                    Console.WriteLine("-------TAREAS DISPONIBLES-------");
+                    foreach(string elemento in lista){
+                        Console.WriteLine(elemento);
+                    }
+                
+                    break;
+
+
+                case 2:
+                    Console.WriteLine("Escribe la tarea que deseas agregar: ");
+                    string tareaPorAgregar = Console.ReadLine();
+                    lista.Add(tareaPorAgregar);
+
+                    break;
+
+                case 3:
+                    Console.WriteLine("Escribe la tarea que deseas eliminar: ");
+                    string tareaPorEliminar = Console.ReadLine();
+
+                    lista.Remove(tareaPorEliminar);
+
+                    break;
+
+
+                case 4: validacion = false; break;
+
+                        
+            }
+
+            Console.ReadKey(true);
+            
+
+        } 
+
+
+        while(validacion);
+    }
+}
